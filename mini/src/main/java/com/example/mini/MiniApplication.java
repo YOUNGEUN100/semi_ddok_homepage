1a2dbf727afd6f65dfeb2ef83f79936bf8bb70c8
pado8 <pado8@DESKTOP-RI6P2S1>
2023-05-03 13:11:25 +0900

    test

--- mini/src/main/java/com/example/mini/MiniApplication.java
+++ mini/src/main/java/com/example/mini/MiniApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MiniApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MiniApplication.class, args);
+		SpringApplication.run(MiniApplication.class, args); 
 	}
 
 }
---
+++ mini/src/main/java/com/example/mini/controller/SampleController.java
@@ -0,0 +1,5 @@
+package com.example.mini.controller;
+
+public class SampleController {
+
+}
