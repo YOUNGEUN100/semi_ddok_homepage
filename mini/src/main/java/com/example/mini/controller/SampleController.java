0f760c582322984d5f58e76a7d85ded36a333851
tj <tj@DESKTOP-OTPGP79>
2023-05-03 19:59:24 +0900

    영은's commit

--- mini/src/main/java/com/example/mini/controller/SampleController.java
+++ mini/src/main/java/com/example/mini/controller/SampleController.java
@@ -1,8 +1,34 @@
 package com.example.mini.controller;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ch.qos.logback.core.model.Model;
+
+import com.google.gson.Gson;
 
 @Controller
 public class SampleController {
 
+	@RequestMapping("/recipe.do")
+	public String recipe(Model model) throws Exception{
+		return "/smart-recipe";
+	}
+	
+	@RequestMapping(value = "/recipe/list.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8") 
+	@ResponseBody
+	public String searchRecipeList(Model model, @RequestParam HashMap <String, Object> map) throws Exception {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("result", "success");
+		return new Gson().toJson(resultMap);
+	}
+	
+	
 }
+		
+	
